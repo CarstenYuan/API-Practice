db85d5bf067d5b7a6229fd0ab83b72c4ce700f0c
Haw Yuan <hawyuan1lv@gmail.com>
2023-03-27 01:19:57 +0800

    Add db.commit(), add return json response, revise user and password of myây local db, and correct the sql syntax for creating the new table

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import mysql.connector
 
 
 # Use local MySQL DB here
 db = mysql.connector.connect(
     host='localhost',
-    user='admin',
-    password='admin',
+    user='root',
+    password='root',
     database='practice'
 )
 
@@ -17,7 +17,7 @@ cursor.execute(
     CREATE TABLE IF NOT EXISTS account (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(32) NOT NULL UNIQUE,
-    password VARCHAR(32) NOT NULL,
+    password VARCHAR(32) NOT NULL
     )
     """
 )
@@ -33,3 +33,12 @@ def create_account():
     # Insert new username and password into the account table
     cursor.execute("INSERT INTO account (username, password) "
                    "VALUES (%s, %s)", (request_data['username'], request_data['password']))
+    db.commit()
+
+    return jsonify({
+        'success': True
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
