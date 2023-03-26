fcc425e390240ba7d3175be2f2ce4f9e832efd6b
Haw Yuan <hawyuan1lv@gmail.com>
2023-03-27 01:08:51 +0800

    Replace wrong SQL command EXIST with EXISTS

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ cursor = db.cursor()
 # Create table if not exists
 cursor.execute(
     """
-    CREATE TABLE IF NOT EXIST account (
+    CREATE TABLE IF NOT EXISTS account (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(32) NOT NULL UNIQUE,
     password VARCHAR(32) NOT NULL,
