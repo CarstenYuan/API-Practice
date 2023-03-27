5418c5f9618e665250f4fd53de770a0137af7858
Haw Yuan <hawyuan1lv@gmail.com>
2023-03-27 22:40:27 +0800

    Move my DB info to .env file and use os and dotenv packages to load the variables

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+from dotenv import load_dotenv
+import os
+
 from flask import Flask, request, jsonify
 import mysql.connector
 
 
 # Use local MySQL DB here
+# Set the environment variables in .env file instead of setting my info here
+load_dotenv()  # load environment variables from .env file
+
 db = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='root',
-    database='practice'
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_DATABASE'),
+    port=os.getenv('DB_PORT')
 )
 
 cursor = db.cursor()
@@ -126,7 +133,6 @@ def verify_account():
     return jsonify({
         'success': True
     })
-
 
 
 if __name__ == '__main__':
